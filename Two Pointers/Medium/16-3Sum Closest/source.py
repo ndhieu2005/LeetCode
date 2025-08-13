cd2926f4fd48273c5bdbdfcc0e7226925f4f06b3
David Hugh <duyhieu0979789818@gmail.com>
2025-08-14 05:11:01 +0700

    updated

---
+++ Two Pointers/Medium/16-3Sum Closest/source.py
@@ -0,0 +1,20 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        n = len(nums)
+        closest_sum = float('inf')
+        for i in range(n-2):
+            left = i + 1
+            right = n-1
+            while left < right:
+                curr_sum = nums[i] + nums[left] + nums[right]
+                if abs(curr_sum - target) < abs(closest_sum - target):
+                    closest_sum = curr_sum
+                if curr_sum > target:
+                    right -= 1
+                elif curr_sum < target:
+                    left += 1
+                elif curr_sum == target:
+                    return target
+        return closest_sum 
+
