763cf43cc96ba04e86e6a4d1452a5a1285281c15
David Hugh <duyhieu0979789818@gmail.com>
2025-08-21 21:14:13 +0700

    updated

---
+++ Stack/Easy/94-Binary Tree Inorder/source.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recursion(self,root,res):
+        if root is not None:
+            self.recursion(root.left,res)
+            res.append(root.val)
+            self.recursion(root.right,res)
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        self.recursion(root,res)
+        return res 
+
+        
